ead382822ffb2c0bbd0d50301f924c5caf75129c
alexogdfx <74707467+alexogdfx@users.noreply.github.com>
2022-12-12 17:26:46 +0000

    Update launch-test.sh

--- launch-test.sh
+++ launch-test.sh
@@ -2,7 +2,7 @@
 
 #testing that container is running
 
-docker container run --detach --publish 80:80 --name server.js alexogdfx/server.js:1.0
+docker container run --detach --publish 80:80 --name server.js alexogdfx/server.js:latest
 
 docker container ls
 
